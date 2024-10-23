b6fbcd3ca2056b4e67ad31d004cc36341999888f
HassanNawazish <hassannawazishrasool1@gmail.com>
2024-10-23 15:49:46 +0200

    adjustment of scene and colors

--- simulation_robot.py
+++ simulation_robot.py
@@ -12,7 +12,7 @@ robot_size = [2, 1, 1]  # Width, length and height of the robot (2m x 1m x 1m)
 sensor_angles_xy = np.linspace(0, 2*np.pi, 8, endpoint=False)
 sensor_angles_z = [-np.pi/4, np.pi/4]  # Sensors tilted upwards and downwards
 
-# Define obstacles in the scene (x, y, z)
+# Define obstacles in the scene (x, y, z) - you can modify this list
 num_obstacles = 10
 obstacles = [(np.random.uniform(-10, 10), np.random.uniform(-10, 10), np.random.uniform(-1, 2)) for _ in range(num_obstacles)]
 
@@ -63,11 +63,15 @@ def visual_simulation():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     
-    ax.set_xlim(-10, 10)
-    ax.set_ylim(-10, 10)
-    ax.set_zlim(-10, 10)
+    # Set axis limits to zoom in
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
+    ax.set_zlim(-2, 5)
     
-    # Draw the robot (parallelepiped)
+    # Set the viewing angle
+    ax.view_init(elev=20, azim=60)
+
+    # Draw the robot (parallelepiped) in green with black edges and no transparency
     robot_points = np.array([[robot_position[0] - robot_size[0]/2, robot_position[1] - robot_size[1]/2, robot_position[2] - robot_size[2]/2],
                              [robot_position[0] + robot_size[0]/2, robot_position[1] - robot_size[1]/2, robot_position[2] - robot_size[2]/2],
                              [robot_position[0] + robot_size[0]/2, robot_position[1] + robot_size[1]/2, robot_position[2] - robot_size[2]/2],
@@ -84,31 +88,31 @@ def visual_simulation():
              [robot_points[j] for j in [1, 2, 6, 5]], 
              [robot_points[j] for j in [4, 7, 3, 0]]]
     
-    ax.add_collection3d(Poly3DCollection(verts, facecolors='blue', linewidths=1, edgecolors='r', alpha=.25))
-    
-    # Draw the sensors (x-y and z planes)
+    ax.add_collection3d(Poly3DCollection(verts, facecolors='green', linewidths=1, edgecolors='black', alpha=1.0))  # Robot color changed to green
+
+    # Draw the sensors (x-y and z planes) in yellow
     for angle in sensor_angles_xy:
         x_sensor = robot_position[0] + np.cos(angle) * 2
         y_sensor = robot_position[1] + np.sin(angle) * 2
         z_sensor = robot_position[2]
-        ax.plot([robot_position[0], x_sensor], [robot_position[1], y_sensor], [robot_position[2], z_sensor], color='green')
-    
+        ax.plot([robot_position[0], x_sensor], [robot_position[1], y_sensor], [robot_position[2], z_sensor], color='yellow')  # Changed color to yellow
+
     for angle_z in sensor_angles_z:
         x_sensor = robot_position[0]
         y_sensor = robot_position[1]
         z_sensor = robot_position[2] + np.sin(angle_z) * 2
-        ax.plot([robot_position[0], x_sensor], [robot_position[1], y_sensor], [robot_position[2], z_sensor], color='orange')
-    
-    # Draw the obstacles (red spheres)
+        ax.plot([robot_position[0], x_sensor], [robot_position[1], y_sensor], [robot_position[2], z_sensor], color='purple')  # Changed color to purple
+
+    # Draw the obstacles (blue spheres)
     for obstacle in obstacles:
-        ax.scatter(obstacle[0], obstacle[1], obstacle[2], color='red', s=200)
+        ax.scatter(obstacle[0], obstacle[1], obstacle[2], color='blue', s=300)  # Changed obstacle color to blue and size to 300
     
     plt.pause(0.1)
     plt.draw()
 
 # Main simulation loop
 def simulation_loop():
-    for _ in range(50):  # Simulate 20 time steps
+    for _ in range(20):  # Simulate 20 time steps
         move_robot()
         plt.clf()  # Clear the previous figure
         visual_simulation()  # Draw the new scene
